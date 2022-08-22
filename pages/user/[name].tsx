690014e782c4d9bc84b3e8e3002af3ee4740c9e9
dawkaka <yousiph77@gmail.com>
2022-08-22 14:30:21 +0000

    profile is now juicy

--- pages/user/[name].tsx
+++ pages/user/[name].tsx
@@ -4,15 +4,12 @@ import styles from "./styles/profile.module.css"
 import { Verified } from "../../components/mis";
 
 export default function Profile() {
-
-    //src="https://p16-sign-va.tiktokcdn.com/tos-maliva-avt-0068/1bbad2972a229f5eedc0dc42e37656f9~c5_100x100.jpeg?x-expires=1661277600&amp;x-signature=2Q528M3b%2B8q54WDWPwZvg4ZwXtI%3D"
-
     return (
         <Layout>
             <section className={styles.section}>
                 <div className={styles.profileTopContainer}>
                     <div className={styles.profileTop}>
-                        <div>
+                        <div className={styles.infoWrapper}>
                             <div className={styles.infoContainer}>
                                 <div className={styles.imageContainer} style={{ width: "116px", height: "116px" }}>
                                     <span className={styles.avatarContainer} style={{ width: "116px", height: "116px" }}>
@@ -45,36 +42,33 @@ export default function Profile() {
                     Dm on Instagram @fatimazawwa for promo`}
                             </h2>
                         </div>
-                        <div data-e2e="user-more" className={styles.actions}><svg width="24" height="24" viewBox="0 0 48 48" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M4 24C4 21.7909 5.79086 20 8 20C10.2091 20 12 21.7909 12 24C12 26.2091 10.2091 28 8 28C5.79086 28 4 26.2091 4 24ZM20 24C20 21.7909 21.7909 20 24 20C26.2091 20 28 21.7909 28 24C28 26.2091 26.2091 28 24 28C21.7909 28 20 26.2091 20 24ZM36 24C36 21.7909 37.7909 20 40 20C42.2091 20 44 21.7909 44 24C44 26.2091 42.2091 28 40 28C37.7909 28 36 26.2091 36 24Z"></path></svg></div>
+                        <div className={styles.actions}><svg width="24" height="24" viewBox="0 0 48 48" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M4 24C4 21.7909 5.79086 20 8 20C10.2091 20 12 21.7909 12 24C12 26.2091 10.2091 28 8 28C5.79086 28 4 26.2091 4 24ZM20 24C20 21.7909 21.7909 20 24 20C26.2091 20 28 21.7909 28 24C28 26.2091 26.2091 28 24 28C21.7909 28 20 26.2091 20 24ZM36 24C36 21.7909 37.7909 20 40 20C42.2091 20 44 21.7909 44 24C44 26.2091 42.2091 28 40 28C37.7909 28 36 26.2091 36 24Z"></path></svg></div>
                     </div>
                 </div>
-                <h1>pic</h1>
-                <h1>pic</h1>
-                <h1>pic</h1>
-                <h1>pic</h1>
-                <h1>pic</h1>
-                <h1>pic</h1>
-                <h1>pic</h1>
-                <h1>pic</h1>
-                <h1>pic</h1>
-                <h1>pic</h1>
-                <h1>pic</h1>
-                <h1>pic</h1>
-                <h1>pic</h1>
-                <h1>pic</h1>
-                <h1>pic</h1>
-                <h1>pic</h1>
-                <h1>pic</h1>
-                <h1>pic</h1>
-                <h1>pic</h1>
-                <h1>pic</h1>
-                <h1>pic</h1>
-                <h1>pic</h1>
-                <h1>pic</h1>
-                <h1>pic</h1>
-                <h1>pic</h1>
-                <h1>pic</h1>
-            </section>
-        </Layout>
+                <div className={styles.profileBottom}>
+                    <div className={styles.showImagesWrapper}>
+                        <article className={styles.showImageContainer}>
+                            <Image src={"/me.jpg"} height="230px" width="200px" layout="responsive" objectFit="cover" className={styles.showImage} />
+                        </article>
+                        <article className={styles.showImageContainer}>
+                            <Image src={"/me2.jpg"} height="230px" width="200px" layout="responsive" objectFit="cover" className={styles.showImage} />
+                        </article>
+                        <article className={styles.showImageContainer}>
+                            <Image src={"/me3.jpg"} height="230px" width="200px" layout="responsive" objectFit="cover" className={styles.showImage} />
+                        </article>
+                        <article className={styles.showImageContainer}>
+                            <Image src={"/me4.jpg"} height="230px" width="200px" layout="responsive" objectFit="cover" className={styles.showImage} />
+                        </article>
+                        <article className={styles.showImageContainer}>
+                            <Image src={"/me5.jpg"} height="230px" width="200px" layout="responsive" objectFit="cover" className={styles.showImage} />
+                        </article>
+                        <article className={styles.showImageContainer}>
+                            <Image src={"/me6.jpg"} height="230px" width="200px" layout="responsive" objectFit="cover" className={styles.showImage} />
+                        </article>
+                    </div>
+                </div>
+
+            </section >
+        </Layout >
     )
 }
