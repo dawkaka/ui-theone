d39f90bfd1cd2fa39c1d3535cab2239562b74897
dawkaka <yousiph77@gmail.com>
2022-08-26 00:41:49 +0000

    only go can judge

--- components/post.tsx
+++ components/post.tsx
@@ -1,17 +1,46 @@
-
-
+import styles from "./styles/post.module.css";
+import Image from "next/image";
+import { Actions } from "./mis";
 interface post {
     userName: string;
     caption: string;
     files: { name: string, type: string }[];
     likes_count: number;
-    comments_count: number
+    comments_count: number;
+    verified: boolean
 }
 
-const Post: React.FunctionComponent<post> = () => {
+const Post: React.FunctionComponent<post> = (props) => {
     return (
         <article>
-
+            <div>
+                <div className={styles.userInfoContainer}>
+                    <div className={styles.infoWrapper}>
+                        <div className={styles.imageContainer} style={{ width: "40px", height: "40px" }}>
+                            <span className={styles.avatarContainer} style={{ width: "40px", height: "40px" }}>
+                                <Image
+                                    layout="fill"
+                                    objectFit="cover"
+                                    src={"/me.jpg"}
+                                    className={styles.profileImage}
+                                />
+                            </span>
+                        </div>
+                        <div>
+                            <h4>John.Doe</h4>
+                        </div>
+                    </div>
+                    <div>
+                        <Actions size={24} orientation="potrait" />
+                    </div>
+                </div>
+                <figure>
+                    <div></div>
+                    <div></div>
+                    <figcaption></figcaption>
+                </figure>
+                <div></div>
+            </div>
 
         </article>
     )
--- pages/r/home.tsx
+++ pages/r/home.tsx
@@ -3,6 +3,7 @@ import styles from './styles/home.module.css'
 import Layout from "../../components/mainLayout";
 import CouplePreview from "../../components/couplepreview";
 import Header from "../../components/pageHeader";
+import Post from "../../components/post";
 
 export default function HomePage() {
     return (
@@ -12,21 +13,7 @@ export default function HomePage() {
                 <section className={styles.postsContainer}>
                     <Header title={"Home"} />
                     <div className={styles.content}>
-                        <CouplePreview profile_picture="/me.jpg" name="Yukieforyoueyesbro" isFollowing={false} status="Dating" verified={false} />
-                        <CouplePreview profile_picture="/me.jpg" name="Yukiesomething" isFollowing={false} status="married" verified />
-                        <CouplePreview profile_picture="/me.jpg" name="Yukbeeie" isFollowing status="married" verified={false} />
-                        <CouplePreview profile_picture="/me.jpg" name="Yukie" isFollowing={false} status="Dating" verified />
-                        <CouplePreview profile_picture="/me.jpg" name="Yukielikesfor" isFollowing status="Dating" verified={false} />
-                        <CouplePreview profile_picture="/me.jpg" name="Yukieforyoueyesbro" isFollowing={false} status="Dating" verified={false} />
-                        <CouplePreview profile_picture="/me.jpg" name="Yukiesomething" isFollowing={false} status="married" verified />
-                        <CouplePreview profile_picture="/me.jpg" name="Yukbeeie" isFollowing status="married" verified={false} />
-                        <CouplePreview profile_picture="/me.jpg" name="Yukie" isFollowing={false} status="Dating" verified />
-                        <CouplePreview profile_picture="/me.jpg" name="Yukielikesfor" isFollowing status="Dating" verified={false} />
-                        <CouplePreview profile_picture="/me.jpg" name="Yukieforyoueyesbro" isFollowing={false} status="Dating" verified={false} />
-                        <CouplePreview profile_picture="/me.jpg" name="Yukiesomething" isFollowing={false} status="married" verified />
-                        <CouplePreview profile_picture="/me.jpg" name="Yukbeeie" isFollowing status="married" verified={false} />
-                        <CouplePreview profile_picture="/me.jpg" name="Yukie" isFollowing={false} status="Dating" verified />
-                        <CouplePreview profile_picture="/me.jpg" name="Yukielikesfor" isFollowing status="Dating" verified={false} />
+                        <Post />
                     </div>
                 </section>
 
