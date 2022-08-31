bd4af24cf888a1d3b6f0cf7091e2c52d30c4cad1
dawkaka <yousiph77@gmail.com>
2022-08-31 21:48:41 +0000

    search modal popup

--- pages/r/explore.tsx
+++ pages/r/explore.tsx
@@ -1,19 +1,30 @@
-
+import { useState } from "react"
 import { FaSearch } from "react-icons/fa"
 import Layout from "../../components/mainLayout"
 import Suggestions from "../../components/suggestions"
 import styles from "../../styles/explore.module.css"
 import { Post } from "../../components/post"
 
+
 export default function Explore() {
+    const [query, setQuery] = useState("")
     return (
         <Layout>
 
             <div className={styles.main}>
                 <section className={styles.exploreContainer}>
                     <div className={styles.searchContainer}>
-                        <input type="search" placeholder="Search el wahid" />
+                        <input type="search" placeholder="Search el wahid" onChange={(e) => {
+                            setQuery(e.currentTarget.value)
+                        }} />
                         <FaSearch className={styles.searchIcon} color="var(--accents-3)" />
+                        {
+                            !!query && (
+                                <div className={styles.searchModal}>
+
+                                </div>
+                            )
+                        }
                     </div>
 
                     <div className={styles.ntfs}>
