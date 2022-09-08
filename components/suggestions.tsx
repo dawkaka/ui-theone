afc3d0efaa9428f1cddd00b4bc934a01d1e468d3
dawkaka <yousiph77@gmail.com>
2022-09-08 00:04:49 +0000

    suggested accounts and cp review make over

--- components/suggestions.tsx
+++ components/suggestions.tsx
@@ -5,7 +5,7 @@ const Suggestions: React.FunctionComponent = () => {
     return (
         <section className={styles.suggestionsContainer}>
             <div className={styles.suggestionsWrapper}>
-                <h1>Suggestions</h1>
+                <h1>Suggested Accounts</h1>
                 <CouplePreview profile_picture="/me.jpg" name="Yukieforyoueyesbro" isFollowing={false} status="dating" verified={false} />
                 <CouplePreview profile_picture="/me.jpg" name="Yukiesomething" isFollowing={false} status="married" verified />
                 <CouplePreview profile_picture="/me.jpg" name="Yukbeeie" isFollowing status="married" verified={false} />
