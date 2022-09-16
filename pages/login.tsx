3d4354a835ecc585eca50cfe43a9907c35bbe292
dawkaka <yousiph77@gmail.com>
2022-09-16 11:36:28 +0000

    login ui

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,10 +1,45 @@
 import { NextPage } from "next";
+import { FormEvent, useState } from "react";
+import styles from "../styles/loginsignup.module.css"
+
 
 
 const Login: NextPage = () => {
-    return (
-        <div>
+    const login = (e: FormEvent) => {
+        e.preventDefault()
+    }
 
-        </div>
+    return (
+        <div className={styles.pageMain}>
+            <main className={styles.main}>
+                <div style={{ background: "url(/med.jpg)", backgroundPosition: "center" }}></div>
+                <div className={styles.formContainer}>
+                    <h2 style={{ textAlign: "center" }}>Login</h2>
+                    <div className={styles.indicatorsContainer}>
+                        <form className={styles.form}>
+                            <div className={styles.formItem}>
+                                <label htmlFor="firstname">Name or email</label>
+                                <input type="text" placeholder="Enter user name or email" name="user_name" required />
+                            </div>
+                            <div className={styles.formItem}>
+                                <label htmlFor="lastname">Password</label>
+                                <input type="password" placeholder="Enter password" name="password" required />
+                            </div>
+                            <div style={{ display: "flex", flexDirection: "column", marginTop: "40px" }}>
+                                <button
+                                    style={{ paddingBlock: "var(--gap-half)" }}
+                                    onClick={login}
+                                >
+                                    Login
+                                </button>
+                            </div>
+
+                        </form>
+                    </div>
+                </div>
+            </main >
+        </div >
     )
 }
+
+export default Login
