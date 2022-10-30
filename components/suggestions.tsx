f7c2361e92212aec03d95c5f6ce24eab7ef2d4c0
dawkaka <yousiph77@gmail.com>
2022-10-30 23:33:40 +0000

    Add remove suggested accounts feature

--- components/suggestions.tsx
+++ components/suggestions.tsx
@@ -4,16 +4,18 @@ import tr from "../i18n/locales/components/suggestions.json"
 import { useRouter } from "next/router"
 import { Langs } from "../types"
 import { BASEURL, IMAGEURL } from "../constants"
-import { useQuery } from "@tanstack/react-query"
+import { useMutation, useQuery } from "@tanstack/react-query"
 import axios from "axios"
 import { Loading } from "./mis"
+import React, { useState } from "react"
+import { AiFillCloseCircle } from "react-icons/ai"
 
 const Suggestions: React.FunctionComponent = () => {
     const router = useRouter()
     const locale = router.locale || "en"
     const localeTr = tr[locale as Langs]
     const { isLoading, data } = useQuery(["suggested"], () => axios.get(`${BASEURL}/couple/u/suggested-accounts`), { staleTime: Infinity })
-
+    console.log(data)
     return (
         <section className={styles.suggestionsContainer}>
             <div className={styles.suggestionsWrapper}>
@@ -23,13 +25,39 @@ const Suggestions: React.FunctionComponent = () => {
                 }
                 {
                     data?.data.map((c: any) => (
-                        <CouplePreview
+                        <PreviewWithRemove node={<CouplePreview
                             profile_picture={`${IMAGEURL}/${c.profile_picture}`}
-                            name={c.couple_name} isFollowing={false} married={c.married} verified={c.verified} />
+                            name={c.couple_name} isFollowing={false} married={c.married} verified={c.verified} />} name={c.couple_name} />
                     ))
                 }
             </div>
         </section>
+    )
+}
+
+const PreviewWithRemove: React.FC<{ node: React.ReactNode, name: string }> = ({ node, name }) => {
+    const [removed, setRemoved] = useState(false)
+    const mutation = useMutation(() => axios.post(`${BASEURL}/user/exempt/${name}`), { onSuccess: () => setRemoved(true) })
+
+    if (removed) {
+        return null
+    }
+    return (
+        <div style={{ display: "flex", flexDirection: "column", position: "relative", marginBottom: "var(--gap-quarter)" }}>
+            {
+                node
+            }
+            <button onClick={() => mutation.mutate()}
+                style={{
+                    position: "absolute",
+                    left: "50%", transform: "translateX(-50%)",
+                    bottom: "-2px",
+                    backgroundColor: "transparent", padding: 0
+                }}
+            >
+                <AiFillCloseCircle size={25} color="var(--accents-2)" />
+            </button>
+        </div>
     )
 }
 
