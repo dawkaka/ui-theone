d71ad7f5c78ef5e2f19ffb0c6666169ab36e3bff
dawkaka <yousiph77@gmail.com>
2022-10-30 14:54:34 +0000

    get suggested accounts

--- components/suggestions.tsx
+++ components/suggestions.tsx
@@ -3,20 +3,31 @@ import CouplePreview from "./couplepreview"
 import tr from "../i18n/locales/components/suggestions.json"
 import { useRouter } from "next/router"
 import { Langs } from "../types"
+import { BASEURL, IMAGEURL } from "../constants"
+import { useQuery } from "@tanstack/react-query"
+import axios from "axios"
+import { Loading } from "./mis"
 
 const Suggestions: React.FunctionComponent = () => {
     const router = useRouter()
     const locale = router.locale || "en"
     const localeTr = tr[locale as Langs]
+    const { isLoading, data } = useQuery(["suggested"], () => axios.get(`${BASEURL}/couple/u/suggested-accounts`), { staleTime: Infinity })
+
     return (
         <section className={styles.suggestionsContainer}>
             <div className={styles.suggestionsWrapper}>
                 <h1>{localeTr.header}</h1>
-                <CouplePreview profile_picture="/me.jpg" name="Yukieforyoueyesbro" isFollowing={false} status="dating" verified={false} />
-                <CouplePreview profile_picture="/me.jpg" name="Yukiesomething" isFollowing={false} status="married" verified />
-                <CouplePreview profile_picture="/me.jpg" name="Yukbeeie" isFollowing status="married" verified={false} />
-                <CouplePreview profile_picture="/me.jpg" name="Yukie" isFollowing={false} status="dating" verified />
-                <CouplePreview profile_picture="/me.jpg" name="Yukielikesfor" isFollowing status="dating" verified={false} />
+                {
+                    isLoading && <Loading size="medium" color="var(--success)" />
+                }
+                {
+                    data?.data.map((c: any) => (
+                        <CouplePreview
+                            profile_picture={`${IMAGEURL}/${c.profile_picture}`}
+                            name={c.couple_name} isFollowing={false} married={c.married} verified={c.verified} />
+                    ))
+                }
             </div>
         </section>
     )
