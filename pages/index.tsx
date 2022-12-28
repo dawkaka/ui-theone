30026ea9c3fc40930e2c0605fbc66bd1a026a7c7
dawkaka <yousiph77@gmail.com>
2022-12-28 20:27:24 +0000

    show rainbow

--- pages/index.tsx
+++ pages/index.tsx
@@ -88,10 +88,10 @@ const LandingPage: NextPage = () => {
   }
 
   useEffect(() => {
-    // const userCountry = getCountry()
-    // if (userCountry) {
-    //   setShowRainbow(!noHomo.includes(userCountry))
-    // }
+    const userCountry = getCountry()
+    if (userCountry) {
+      setShowRainbow(!noHomo.includes(userCountry))
+    }
     if (localStorage.getItem("hasAccount")) {
       router.replace("/r/home")
     }
