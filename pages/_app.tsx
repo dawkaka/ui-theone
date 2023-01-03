bfc642079b8d5fad40e7e386d6a1bece37927b7f
dawkaka <yousiph77@gmail.com>
2023-01-03 17:05:20 +0000

    fix: move default headers to _document

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -41,18 +41,6 @@ function MyApp({ Component, pageProps }: AppProps) {
 
   return (
     <ToasContext.Provider value={notify}>
-      <Head>
-        <link rel="icon" href="/favicon.ico" />
-        <meta charSet="utf-8" />
-        <meta httpEquiv="Content-Security-Policy" content="upgrade-insecure-requests" />
-        <meta httpEquiv="content-type" content="text/html; charset=UTF-8" />
-        <meta name="referrer" content="always" />
-        <meta httpEquiv="X-UA-Compatible" />
-
-        <meta name="twitter:label1" content="Prime Couples" />
-        <meta name="twitter:domain" content="primecouples.com" />
-        <meta name="twitter:card" content="summary_large_image" />
-      </Head>
       <QueryClientProvider client={queryClient}>
         <Component {...pageProps} />
         <Toast message={message} type={type} resetMessage={() => setMessage("")} />
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,6 +8,17 @@ export default function Document() {
                 <link rel="apple-touch-icon" href="/icon.png" />
                 <meta name="theme-color" content="#fe2c55" />
 
+                <link rel="icon" href="/favicon.ico" />
+                <meta charSet="utf-8" />
+                <meta httpEquiv="Content-Security-Policy" content="upgrade-insecure-requests" />
+                <meta httpEquiv="content-type" content="text/html; charset=UTF-8" />
+                <meta name="referrer" content="always" />
+                <meta httpEquiv="X-UA-Compatible" />
+
+                <meta name="twitter:label1" content="Prime Couples" />
+                <meta name="twitter:domain" content="primecouples.com" />
+                <meta name="twitter:card" content="summary_large_image" />
+
             </Head>
             <body>
                 <Main />
